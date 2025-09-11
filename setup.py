d70fd2147eaccaf53a1c47fa695c8ba7b6c3653c
Khizer-khan701 <khizerdawood987@gmail.com>
2025-09-11 17:55:02 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="Medical_Chatbot",
+    version="0.1.0",
+    author="Khizer Dawood",
+    author_email="khizerkhan81009@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
